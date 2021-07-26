f0637517e8816a28bb7a4072f701a29c1ecbe65b
Sanju <49448546+Sanju-S-S@users.noreply.github.com>
2021-07-26 11:45:44 +0530

    Navbar Overview

--- sakhyaa_info/src/App.js
+++ sakhyaa_info/src/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import Logo from './resources/Logo.jpg';
-import { Layout, Menu } from 'antd';
-
-const { Header, Content, Footer } = Layout;
+import Navbar from './Navbar';
 
 
 function App() {
   return (
-    <div className="App">
-      <Layout className="layout">
-    <Header>
-      <div/>
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-      <Menu.Item key="1">
-        <img src={Logo} alt="Logo" style={{height:68, width:175}}/>
-      </Menu.Item>
-      <Menu.Item key="3" style={{marginRight : "2%", marginLeft:"Auto" }}>
-              <a href="https://www.google.com">Register</a>
-      </Menu.Item>
-      </Menu>
-    </Header>
-    <Content>
-      <div className="site-layout-content"><h1>Content</h1></div>
-
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>Designed and Developed by Sakhyaa &#9829;</Footer>
-  </Layout>
-    </div>
+    <Navbar/>
   );
 }
 
